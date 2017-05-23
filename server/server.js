b649c6ea61eaeaba58c702e3c46600534ef7e4ee
Sem Ekkelboom <sem.ekkelboom@gmail.com>
2017-05-23 14:25:10 +0200

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,34 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // validate id using isvalid
+  if (!ObjectID.isValid(id)) {
+    // respond with 404 - send back empty body
+    res.status(404).send();
+  }
+
+  // findbyid
+  Todo.findById(id).then((todo) => {
+    // Success
+    if (todo) {
+      // If todo - send it back
+      res.status(200).send(todo);
+    } else if (!todo) {
+      // If no todo - Send back 404 with empty body
+      res.status(404).send();
+    }
+  }, (e) => {
+    // Error
+    // Send back 400 - send back empty body
+    res.status(400).send();
+  });
+
 });
 
 app.listen(3000, () => {
