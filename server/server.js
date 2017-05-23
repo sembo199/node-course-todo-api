c797dca6526bc5e8ca0b866545e20a3eed39a0a1
Sem Ekkelboom <sem.ekkelboom@gmail.com>
2017-05-23 14:46:30 +0200

    Add tests GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -31,32 +31,22 @@ app.get('/todos', (req, res) => {
   });
 });
 
-// GET /todos/:id
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  // validate id using isvalid
   if (!ObjectID.isValid(id)) {
-    // respond with 404 - send back empty body
-    res.status(404).send();
+    return res.status(404).send();
   }
 
-  // findbyid
   Todo.findById(id).then((todo) => {
-    // Success
-    if (todo) {
-      // If todo - send it back
-      res.status(200).send(todo);
-    } else if (!todo) {
-      // If no todo - Send back 404 with empty body
-      res.status(404).send();
+    if (!todo) {
+      return res.status(404).send();
     }
-  }, (e) => {
-    // Error
-    // Send back 400 - send back empty body
+
+    res.send({todo});
+  }).catch((e) => {
     res.status(400).send();
   });
-
 });
 
 app.listen(3000, () => {
