2509572ee70de66ef01ac334f6a38c6cfd628fb9
Sem Ekkelboom <sem.ekkelboom@gmail.com>
2017-05-25 15:31:07 +0200

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -40,6 +40,7 @@ UserSchema.methods.toJSON = function () {
   return _.pick(userObject, ['_id', 'email']);
 };
 
+// Methods to call on instance.
 // Ik maak hier geen gebruik van ES6 arrow functions, omdat ik this nodig heb.
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
@@ -52,6 +53,24 @@ UserSchema.methods.generateAuthToken = function () {
     return token;
   });
 };
+
+// Static method to call on non-object
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+}
 
 var User = mongoose.model('User', UserSchema);
 
